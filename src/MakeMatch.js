b90e44410b3258cd9653ede63f2de7307807e9ee
Benjamin Cheng <ben@bcheng.cf>
2017-04-02 03:15:13 -0400

    History

--- src/MakeMatch.js
+++ src/MakeMatch.js
@@ -2,7 +2,50 @@ import React, {Component} from 'react';
 import * as firebase from 'firebase';
 
 class MakeMatch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            game: {},
+            bot1: {},
+            bot2: {},
+            history: [],
+        };
+    }
+
+    addHistory(move) {
+        let history = this.state.history.slice();
+        history.push(move);
+        console.log(history);
+        this.setState({
+            ...this.state,
+            history: history
+        });
+    }
+
+    setGame(game) {
+        let gameref = firebase.database().ref("/games/" + game);
+        gameref.on("value", snap => {
+            if (snap.val() != null) {
+                console.log(snap.val());
+                this.setState({
+                    game: game,
+                    bot1: snap.val().bot1,
+                    bot2: snap.val().bot2,
+                    history: [],
+                });
+
+                gameref.off("value");
+                gameref.child("history").on("child_added", move => this.addHistory(move.val()));
+            }
+        });
+    }
+
     handleClick = () => {
+        const bot = {
+            elo: 123123,
+            lang: "lua",
+            code: 'while true do print(2) end'
+        };
         fetch('/api/match', {
             method: 'post',
             headers: {
@@ -10,13 +53,9 @@ class MakeMatch extends Component {
             },
             body: JSON.stringify({
                 uid: firebase.auth().currentUser.uid,
-                ai: {
-                    elo: 123123,
-                    lang: "lua",
-                    code: 'while true do print(1) end'
-                }
+                ai: bot,
             })
-        }).then(res => res.text().then(res => console.log(res)));
+        }).then(res => res.text().then(res => this.setGame(JSON.parse(res).key)));
     }
     
     render() {
@@ -24,6 +63,6 @@ class MakeMatch extends Component {
             <button type="submit" onClick={this.handleClick} className="button">Make Match</button>
         );
     }
-}
+}1
 
 export default MakeMatch;
